bd3217e6afaf9a3573bd9bee29820eb6cea1897e
SanjalaSadini <sadini95kumarasiri@gmail.com>
2019-09-20 12:32:09 +0530

    Dropdown

--- app/src/main/java/com/example/hotelreservation/ResturentActivity.java
+++ app/src/main/java/com/example/hotelreservation/ResturentActivity.java
@@ -16,9 +16,9 @@ public class ResturentActivity extends AppCompatActivity {
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(ResturentActivity.this,
-                android.R.layout.simple_expandable_list_item_1, getResources().getStringArray(R.array.names));
+                R.layout.spinner, getResources().getStringArray(R.array.names));
 
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myAdapter.setDropDownViewResource(R.layout.spinner);
         mySpinner.setAdapter(myAdapter);
     }
 }
