31fb34ba08b0105942f3bbdac40b33fb210572b4
SanjalaSadini <sadini95kumarasiri@gmail.com>
2019-09-27 01:40:44 +0530

    Delete service Data

--- app/src/main/java/com/example/hotelreservation/ViewDetailsServiceActivity.java
+++ app/src/main/java/com/example/hotelreservation/ViewDetailsServiceActivity.java
@@ -22,7 +22,7 @@ public class ViewDetailsServiceActivity extends AppCompatActivity {
 
     private EditText room, phone, name, timeEdit, service;
     Button showBtn, editBtn;
-    private static DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("Service-Information").child("1");
+    private static DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("Service-Information").child("4");
 
 
     @Override
@@ -86,46 +86,12 @@ public class ViewDetailsServiceActivity extends AppCompatActivity {
         deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                deleteData();
+              deleteData();
             }
         });
-//        editBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                database= FirebaseDatabase.getInstance().getReference().child("Service-Information").child("1");
-//
-//                String enterPhoneNumber=phone.getText().toString();
-//                String enterName=name.getText().toString();
-//                String enterService=service.getText().toString();
-//                String enterTime=timeEdit.getText().toString();
-////                String ste = room.getText().toString();
-////                dataSnapshot.getRef().child("roomNumber").setValue(ste);
-//
-//                editData(enterPhoneNumber,enterName,enterService,enterTime);
-//
-//            }
-//        });
+
     }
 
-    private void editData(final String strPhone, String strName, String strService, final String strTime) {
-
-        Query editQuery = database.orderByChild("phone").equalTo(strPhone);
-        editQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot edtData : dataSnapshot.getChildren()) {
-                    edtData.getRef().child("Service-Information").setValue(strTime);
-                    dataSnapshot.getRef().child("roomNumber").setValue(strPhone);
-                }
-                Toast.makeText(ViewDetailsServiceActivity.this, "Data Edited", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
 
     public void showData() {
         database.addValueEventListener(new ValueEventListener() {
